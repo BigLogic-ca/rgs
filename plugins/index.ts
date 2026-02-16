f25bd8bdceed405ec86559efcd9ffd5b3f5a6d12
Dario Passariello <dariopassariello@gmail.com>
2026-02-16 16:23:15 -0700

    Update index.ts

--- plugins/index.ts
+++ plugins/index.ts
@@ -12,6 +12,13 @@ export { guardPlugin } from "./official/guard.plugin"
 export { analyticsPlugin } from "./official/analytics.plugin"
 export { syncPlugin } from "./official/sync.plugin"
 export { debugPlugin } from "./official/debug.plugin"
+export { indexedDBPlugin } from "./official/indexeddb.plugin"
+export {
+  cloudSyncPlugin,
+  createMongoAdapter,
+  createFirestoreAdapter,
+  createSqlRestAdapter
+} from "./official/cloud-sync.plugin"
 
 import type { PluginContext, IPlugin } from "../core/types"
 
