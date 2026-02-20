8316c3b03d958ba101778472f17f41cf4436713e
Dario Passariello <dariopassariello@gmail.com>
2026-02-19 20:34:46 -0700

    Update api.test.ts

--- tests/playwright/tests/api.test.ts
+++ tests/playwright/tests/api.test.ts
@@ -1,7 +1,12 @@
 import { test } from '@playwright/test'
 import fs from 'node:fs'
 import path from 'node:path'
-const pk = JSON.parse(fs.readFileSync(path.resolve(path.resolve(), '../app.json'), 'utf-8'))
+import { fileURLToPath } from 'url'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
+const pk = JSON.parse(fs.readFileSync(path.resolve(__dirname, '../../../app.json'), 'utf-8'))
 
 // Configure baseURL for this file.
 test.use({ baseURL: pk.app.host.prod })
