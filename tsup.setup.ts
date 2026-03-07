202c2fa65befafc6b1745362ae32c7e71d2560db
Dario Passariello <dariopassariello@gmail.com>
2026-03-06 17:22:08 -0700

    Create tsup.setup.ts

---
+++ tsup.setup.ts
@@ -0,0 +1,17 @@
+export const include = [
+  { from: '.github/COPYRIGHT.md', to: 'COPYRIGHT.md' },
+  { from: '.github/LICENSE.md', to: 'LICENSE.md' },
+  { from: '.github/SECURITY.md', to: 'SECURITY.md' },
+  { from: '.github/FUNDING.yml', to: 'FUNDING.yml' },
+  { from: 'docs/README.md', to: 'README.md' },
+  { from: 'package.json', to: 'package.json' },
+  { from: 'types/', to: 'types/' },
+  //! { from: 'index.d.ts', to: 'index.d.ts' },
+  { from: 'docs/**/*', to: 'docs/' }
+]
+
+export const plug = {
+  copyStatic: true,
+  injectCss: true,
+  types: true
+}
