1e478597819bdf840b13feb3ccad2668a75f0482
Dario Passariello <dariopassariello@gmail.com>
2026-03-22 19:56:25 -0600

    Create jest.config.cjs

---
+++ tests/jest/jest.config.cjs
@@ -0,0 +1,23 @@
+/** @type {import('jest').Config} */
+module.exports = {
+  testEnvironment: 'jsdom',
+  rootDir: '../../',
+  displayName: 'CLIENT',
+  verbose: true,
+  roots: ['<rootDir>'],
+  testMatch: ['<rootDir>/tests/jest/tests/**/*.test.ts'],
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json'],
+  setupFilesAfterEnv: ['<rootDir>/tests/jest/jest.setup.ts'],
+  transform: {
+    '^.+\\.tsx?$': '<rootDir>/tests/node_modules/ts-jest'
+  },
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/$1',
+    '^(\\.{1,2}/.*)\\.js$': '$1'
+  },
+  modulePathIgnorePatterns: ['<rootDir>/dist/'],
+  testPathIgnorePatterns: ['/node_modules/', '<rootDir>/dist/'],
+  transformIgnorePatterns: [
+    '/node_modules/(?!(memorio|immer|dphelper.types)/)'
+  ]
+}
