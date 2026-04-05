12555cfa918504dcd0f9e22b5d735d022bdc4307
Dario Passariello <dariopassariello@gmail.com>
2026-04-05 01:34:25 -0600

    Update npmUpdate.js

--- .dev/node/npmUpdate.js
+++ .dev/node/npmUpdate.js
@@ -1,7 +1,7 @@
 const execSync = require("node:child_process").execSync
 
-execSync("cd ../.. && npx npm-check-updates -u && npm i --force", { stdio: [0, 1, 2] })
-execSync("cd ../../tests && npx npm-check-updates -u && npm i --force", { stdio: [0, 1, 2] })
+execSync("cd ../.. && npx npm-check-updates -u && npm i --force && npm audit fix -f", { stdio: [0, 1, 2] })
+execSync("cd ../../tests && npx npm-check-updates -u && npm i --force && npm audit fix -f", { stdio: [0, 1, 2] })
 execSync("cd ../../vscode-extension && npx npm-check-updates -u && npm i --force", { stdio: [0, 1, 2] })
 
 execSync("node changeVersion.js", { stdio: [0, 1, 2] })
